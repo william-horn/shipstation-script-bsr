c22477671c3d0c093207cee889ad1edf66897a84
William Horn <98601452+william-horn@users.noreply.github.com>
2024-06-26 12:35:37 -0400

    Update shipstation-assistant.js
    rounding issue spoted

--- shipstation-assistant.js
+++ shipstation-assistant.js
@@ -61,7 +61,14 @@ const getNumberComponents = (number, scaleRemainder) => {
   const whole = Math.trunc(number);
   let remaining = Math.abs(number) - whole;
 
+  /*
+   ! issue: 
+   In cases where we have a repeating decimal as a result from the division,
+   for example: 4/12 (0.3333...), subtracting the whole number part from that
+   will cause weird rounding logic where 4/12 won't equal a full third.
+  */
   if (typeof(scaleRemainder) === 'number') {
+   // this is a temporary fix
     remaining = Math.floor(remaining*scaleRemainder + 0.0001);
   }
 
